35917e3a75b9bb7164715ce35333041b1f41780c
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-10-25 15:17:30 -0600

    Profile multiple zoom levels (#21)
    * Allow specifying zoom level
    
    * Add file format to notebook name
    
    * Don't rechunk data
    
    * Load data into memory to avoid topology error
    
    * Add example using sparse for applying weights
    
    * Update execution notebooks
    
    * Update results
    
    * Update index

--- examples/utils.py
+++ examples/utils.py
@@ -17,8 +17,10 @@ def get_files(folder):
 
 def get_test_info_from_filename(file):
     results_base = file.split(".")[0]
-    _, dataset, task, method, driver, virtualization = results_base.split("-")
-    return dataset, task, method, driver, virtualization
+    _, dataset, task, data_format, method, driver, virtualization, zoom = (
+        results_base.split("-")
+    )
+    return dataset, task, data_format, method, driver, virtualization, zoom
 
 
 def load_data(filepath):
@@ -34,9 +36,11 @@ def process_results(summary_dir):
         (
             df.loc[ind, "dataset"],
             df.loc[ind, "task"],
+            df.loc[ind, "format"],
             df.loc[ind, "method"],
             df.loc[ind, "driver"],
             df.loc[ind, "virtual"],
+            df.loc[ind, "zoom"],
         ) = get_test_info_from_filename(filename)
         memray_data = load_data(f"{summary_dir}/{filename}")
         df.loc[ind, "peak memory (GB)"] = memray_data["metadata"]["peak_memory"] * 1e-9
